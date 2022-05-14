efca7a760b6892fad5834648a2865e21ddeba3c0
SimonvanLoon <s.p.c.a.van.loon@student.rug.nl>
2022-05-14 17:00:09 +0200

    second commit. Added first functioning if function.

--- assignment_2/js/main.js
+++ assignment_2/js/main.js
@@ -1,8 +1,28 @@
 function validateForm(){
-    console.log('hello');
+    $("#form-alert").hide();
+    let dataObject = {}
+    if (!/[^a-z]/i.test($("#InputName").val()) === false || !/[^a-z]/i.test($("#InputCity").val()) === false){
+        $("#form-alert").text("Your name and city can only contain letters.").show();
+        console.log(!/[^a-zA-Z]/.test($("#InputName").val()))
+        return
+    }
+    // if (/^\d+$/.test($("#InputAge").text()) === true || !/[^a-zA-Z]/.test($("#InputPhoneNumber").text()) === true){
+    //     console.log("testddd");
+    // }
+    $("form").children("div").each(function() {
+        if ($(this).children('input').val() === ""){
+            console.log("error")
+            $("#form-alert").text("n").show();
+            return
+        }
 
-}
+        dataObject[$(this).children('label').text()] = $(this).children('input').val();
+        console.log($(this).children('label').text())
+        console.log($(this).children('input').val())
+    })
+    console.log(dataObject)
 
+};
 function writeFormData(){
 
 }
